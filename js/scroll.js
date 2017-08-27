43cd79fc094b802749f8ea79e40d5b887fab52d1
Magda Wielgosz <wielgosz.m@vp.pl>
2017-08-27 12:49:18 +0200

    test scrollY

--- js/scroll.js
+++ js/scroll.js
@@ -2,6 +2,10 @@
 
 $(function () {
     $(window).scroll(function () {
-        console.log("test");
+        console.log("test" + scrollY);
     });
 });
+
+
+//scrollY - zwraca tyle pix ile przescrollowaliśmy
+//innerheight - zwraca wysokość tak jak jest okno
